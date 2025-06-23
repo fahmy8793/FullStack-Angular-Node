8e7c350365bec8b0d9c6ce87fd6fb899633a50fa
Mohamed Ramadan <mhmdrmdan652@gmail.com>
2025-06-24 02:36:36 +0300

    header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from "./core/layout/header/header.component";
+import { FooterComponent } from "./core/layout/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
