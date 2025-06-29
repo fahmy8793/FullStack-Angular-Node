52ada7221e2b3aed978e14b0d9112a2168942ed3
AmiraEltaher <amiraeltaher2020@gmail.com>
2025-06-29 13:54:35 +0300

    forget password component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { CartComponent } from './users/cart/cart.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'forgot-password' , component: ForgetPasswordComponent},
 ];
