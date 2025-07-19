c225a5e9da4c761eb038ea67bfc1985d0505b547
fahmy8793 <a.fahmy@graduate.utm.my>
2025-07-19 23:49:11 +0300

    admin navgition + routes

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,28 @@
+// services/admin.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private apiUrl = `${environment.apiUrl}/admin`;
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  // Get dashboard stats
+  getDashboardStats(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/dashboard`, {
+      headers: this.getHeaders()
+    });
+  }
+}
